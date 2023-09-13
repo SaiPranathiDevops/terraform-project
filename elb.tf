d6d15515c60a649c33ef05dbb9372297fc6d2a15
SaiPranathiDevops <141357756+SaiPranathiDevops@users.noreply.github.com>
2023-09-13 11:03:02 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
-  availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
+  name               = "pranathi-terraform-elb"
+  availability_zones = ["ap-southeast-2a", "ap-southeast-2b"]
 
   listener {
     instance_port     = 80
@@ -21,7 +21,7 @@ resource "aws_elb" "bar" {
   cross_zone_load_balancing = true
   idle_timeout              = 400
   tags = {
-    Name = "raham-tf-elb"
+    Name = "pranathi-tf-elb"
   }
 }
 
@@ -31,7 +31,7 @@ resource "aws_elb" "bar" {
   engine_version          = "5.7.mysql_aurora.2.11.2"
   availability_zones      = ["ap-southeast-1a", "ap-southeast-1b"]
   database_name           = "mydb"
-  master_username         = "raham"
+  master_username         = "pranathi"
   master_password         = "Rahamshaik#444555"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
